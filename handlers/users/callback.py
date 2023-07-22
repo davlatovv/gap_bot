123b065c8fb304ccd4337f4a62710aead8408696
z.davlatov <z.davlatov@atmos.uz>
2023-07-22 19:38:08 +0500

    callback UPDATE text

--- handlers/users/callback.py
+++ handlers/users/callback.py
@@ -2,8 +2,6 @@ from aiogram.types import CallbackQuery
 from aiogram.utils.json import json
 
 from loader import dp, bot, _
-from text import you_have_successfully_switched_now_its_your_turn, something_went_wrong, you_click_to_button_no, \
-    you_denied
 from utils.db_api.db_commands import DBCommands
 
 
@@ -14,22 +12,22 @@ async def handle_yes_button(callback_query: CallbackQuery):
     group = data['group']
     if await DBCommands.change_queue(user_id_from=callback_query.from_user.id, user_id_to=from_user, group_id=group):
         user_queue = await DBCommands.get_user_from_member(group_id=group, user_id=callback_query.from_user.id)
-        await bot.send_message(chat_id=int(callback_query.from_user.id), text=_(you_have_successfully_switched_now_its_your_turn) + str(user_queue.id_queue))
+        await bot.send_message(chat_id=int(callback_query.from_user.id), text=_("Вы успешно поменялись, теперь ваша очередь ") + str(user_queue.id_queue))
         to_user_queue = await DBCommands.get_user_from_member(group_id=group, user_id=from_user)
-        await bot.send_message(chat_id=int(from_user), text=_(you_have_successfully_switched_now_its_your_turn) + str(to_user_queue.id_queue))
+        await bot.send_message(chat_id=int(from_user), text=_("Вы успешно поменялись, теперь ваша очередь ") + str(to_user_queue.id_queue))
         await bot.delete_message(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id)
     else:
-        await callback_query.answer(_(something_went_wrong))
+        await callback_query.answer(_("Что-то пошло не так: "))
         await bot.delete_message(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id)
 
 
 @dp.callback_query_handler(lambda callback_query: callback_query.data[10:12] == 'no', state="*")
 async def handle_no_button(callback_query: CallbackQuery):
-    await callback_query.answer(_(you_click_to_button_no))
+    await callback_query.answer(_("Вы нажали кнопку 'Нет'"))
     data = eval(callback_query.data)
     from_user = data['from_user']
     await bot.send_message(chat_id=int(from_user),
-                           text=_(you_denied))
+                           text=_("Вам отказали"))
     await bot.delete_message(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id)
 
 
